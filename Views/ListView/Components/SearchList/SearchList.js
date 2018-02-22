5bd3cb4cc3a92aaba2443a27e3528186b14a09aa
anonymous <anonymous@anonymous-PC>
2018-02-22 15:45:05 +0500

    SearchComp Action Finalized, But Implementation

--- Views/ListView/Components/SearchList/SearchList.js
+++ Views/ListView/Components/SearchList/SearchList.js
@@ -22,10 +22,14 @@ var SBPosition={};
 SearchList=function(){
     var app = angular.module('SearchListApp', []);
     app.controller('SearchCtrl', function($scope) {
-        $scope.ListColumnIds = ListSettings.CurrentListView.ListViewSettings.ColumnsWithOrder;
-        $scope.ListColumnDetails = ListSettings.ListColumns;
-        $scope.RenderSearchBoxAdv = TempViewSettings.SearchListComp.Render.SearchBoxAdv;
+        $scope.HoldListColumnIds = ListSettings.CurrentListView.ListViewSettings.ColumnsWithOrder; //hold original ListIds 
+        $scope.ListColumnIds = ListSettings.CurrentListView.ListViewSettings.ColumnsWithOrder; //these ids behaves as controlling unit for query display
+        $scope.ListColumnDetails = ListSettings.ListColumns;//
+        $scope.RenderSearchBoxAdv = TempViewSettings.SearchListComp.Render.SearchBoxAdv;//not used
+        $scope.SearchTemplate="CustomCopySearchBox"; //this variabel stores the information of template being copied (SB,ColHeader,Raw)
+        $scope.FilterExcludedCol=[];//It cols being excluded from query in SB,ColHeader mode. Whereas its not valid for RawCustomn
         $scope.SyncScope=function(e){
+                    $scope.HoldListColumnIds = ListSettings.CurrentListView.ListViewSettings.ColumnsWithOrder;
                     $scope.ListColumnIds = ListSettings.CurrentListView.ListViewSettings.ColumnsWithOrder;
                     $scope.ListColumnDetails = ListSettings.ListColumns;
                     $scope.RenderSearchBoxAdv = TempViewSettings.SearchListComp.Render.SearchBoxAdv;
@@ -69,25 +73,41 @@ SearchList=function(){
 
         }
         $scope.ListenerCustomQueryStarter=function(e){
+            $scope.ListColumnIds = ListSettings.CurrentListView.ListViewSettings.ColumnsWithOrder;
             if ($(e.target).hasClass("CustomCopySearchBox")){
                 // OR Operated Query will be copied and reflected in Adv Search 
+                $scope.SearchTemplate="CustomCopySearchBox";
                 $scope.ActionReflectMainSBQuery(e);
                 $scope.ActionComputeAdvSearchQuery(e);
                 
             }
             else if ($(e.target).hasClass("CustomCopyColHeader")){
                 // AND Operated Query will be copied and reflected in Adv Search
+                $scope.SearchTemplate="CustomCopyColHeader";
                 $scope.ActionReflectColHdrQuery(e);
                 $scope.ActionComputeAdvSearchQuery(e);
                 
             }
             else if ($(e.target).hasClass("CustomSearchRaw")){
                 // Custom Query can be added here
-                
+                $scope.SearchTemplate="CustomSearchRaw";
+                $scope.ListColumnIds=[];
+                $scope.ListColumnIds[0] = $scope.ListColumnIds[1];
             }
         }
         $scope.ActionComputeAdvSearchQuery=function(){
-            
+            var QueryRows=$(".QueryForCols")
+            var Query="START "
+            for (var i=0; i<QueryRows.length;i++){
+                Query=Query+$(".QueryForCols")[i].id.split("QueryForCol")[1]+" ";
+                Query=Query+$($(".QueryForCols")[0]).find(".SearchCondition select").val()+" ";
+                Query=Query+$($(".QueryForCols")[0]).find(".SearchFor input").val()+" ";
+                Query=Query+$($(".QueryForCols")[0]).find(".SearchLogic select").val()+" ";
+            }
+            Query=Query+"STOP"
+            TempViewSettings.SearchListComp.SearchBoxQ.CustomQuery=Query;
+            //START 12 Contains 'Hello' 32 AND 24 Contains 'Test'  
+            //alert(Query);
         }
         $scope.ActionReflectMainSBQuery=function(e){
             for(i=0;i<$scope.ListColumnIds.length;i++){
@@ -111,6 +131,39 @@ SearchList=function(){
             alert(RenderSearchBoxAdv);
             return $scope.firstName + " " + $scope.lastName;
         };
+        $scope.ActionToggleCurrFilterItem=function(e){
+            e.stopPropagation();
+            var ToggleRow=parseInt($(e.target).parents("li").attr("id").split("QueryForCol")[1]);
+            var Index=$scope.FilterExcludedCol.indexOf(ToggleRow)
+            
+            if (Index<0){// exclude in query
+                $scope.FilterExcludedCol.push(ToggleRow)
+            }
+            else{//include in query
+                $scope.FilterExcludedCol=$scope.FilterExcludedCol.filter(function(item){
+                                                                        return item!==ToggleRow
+                                                                   })
+            }
+            
+        }
+        
+        $scope.ActionRemoveCurrFilterItem=function(e){
+            e.stopPropagation();
+            var RemoveRow=parseInt($(e.target).parents("li").attr("id").split("QueryForCol")[1]);
+            $scope.ListColumnIds= $scope.ListColumnIds.filter(function(item) { return item !== RemoveRow})
+        }
+       
+        
+        
+        $scope.HelperSelectEventCapture=function(e){
+            e.stopPropagation();
+            $scope.HoldFocusEvent=e;
+        }
+        $scope.ActionTagParentQueryElement=function(e){
+            e=$scope.HoldFocusEvent;
+            $(e.target).parents("li").attr("id","QueryForCol"+$(e.target).val());
+        }
+        
     });
 //END Angular##########################################################################################################################
 
